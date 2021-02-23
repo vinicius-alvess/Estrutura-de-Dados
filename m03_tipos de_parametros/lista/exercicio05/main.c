e56f9019b47b1e1fb66e2249f32c0bca7d47a635
Vinícius Alves de Sousa <77307847+viniciusalves999@users.noreply.github.com>
2021-02-23 16:17:39 -0300

    Update main.c exercício 5

--- m03_tipos de_parametros/lista/exercicio05/main.c
+++ m03_tipos de_parametros/lista/exercicio05/main.c
@@ -14,18 +14,21 @@ Obs: a agenda deve ter capacidade para 100 entradas.
 */
 
 #include <stdio.h>
-#define limite 100 // limite de cadastros
+#define limite 100 // constante limite de cadastros
 
-int n_cadastros = 0;
-long int cep[limite];
-char nome[limite][30], endereco[limite][100], fone[limite][10];
+//VARIAVEIS GLOBAIS
+char nome[limite][30], endereco[limite][100];
+long int cep[limite], fone[limite];
+static int n_cadastro = 0;
 
+//FUNCOES
 void cadastro();
+void imprime_dados_cadastrados();
+void pesquisa_imprime();
 
 void cadastro()
 {
     char *pnome, *pendereco, *pfone, *pcep;
-    static int n_cadastro = 0;
 
     //NOME
     printf("Nome: ");
@@ -43,7 +46,7 @@ void cadastro()
 
     //TELEFONE
     printf("Telefone: ");
-    scanf("%s", &fone[n_cadastro]);
+    scanf("%ld", &fone[n_cadastro]);
     printf("\n");
 
     pfone = &fone[n_cadastro];
@@ -58,39 +61,88 @@ void cadastro()
     n_cadastro++;
 }
 
+void imprime_dados_cadastrados()
+{
+    int i;
+
+    printf("Cadastros:\n\n");
+    printf("------------------------------\n");
+
+    for (i = 0; i < n_cadastro; i++)
+    {
+        printf("Nome: %s\n", nome[i]);
+        printf("Endereco: %s\n", endereco[i]);
+        printf("Telefone: %ld\n", fone[i]);
+        printf("CEP: %ld\n", cep[i]);
+        printf("------------------------------\n\n");
+    }
+}
+
+void pesquisa_imprime()
+{
+    int i, j = 0;
+    char pesquisa;
+
+    printf("Pesquisar pela letra: \n");
+    scanf("%s", &pesquisa);
+    printf("\n");
+
+    for (i = 0; i < n_cadastro; i++)
+    {
+        if (pesquisa == nome[i][0])
+        {
+            printf("Nome: %s\n", nome[i]);
+            printf("Endereco: %s\n", endereco[i]);
+            printf("Telefone: %ld\n", fone[i]);
+            printf("CEP: %ld\n", cep[i]);
+            printf("------------------------------\n\n");
+            j = 1;
+        }
+    }
+    printf("\n");
+    if (j == 0)
+    {
+        printf("Nao existe um nome cadastrado que comeca com essa letra\n\n");
+    }
+}
+
 int main()
 {
-    char opcao;
-
-    printf("a. Entrar um novo nome na agenda.\n");
-    printf("b. Imprimir na tela os dados de todas as pessoas cadastradas.\n");
-    printf("c. Imprimir a lista de nomes cadastrados que comecem por uma letra informada pelo usuario.\n");
-    printf("d. Fim\n");
-
-    scanf("%c", &opcao);
-
-    switch (opcao)
+    int opcao;
+    do
     {
-    case 'a':
-        cadastro();
-        break;
+        printf("1. Entrar um novo nome na agenda.\n");
+        printf("2. Imprimir na tela os dados de todas as pessoas cadastradas.\n");
+        printf("3. Imprimir a lista de nomes cadastrados que comecem por uma letra informada pelo usuario.\n");
+        printf("4. Fim\n");
+        printf("\n");
+        printf("Escolha uma opcao: \n");
+        scanf("%d", &opcao);
+        printf("\n");
 
-    case 'b':
-        break;
+        switch (opcao)
+        {
+        case 1:
+            cadastro();
+            break;
 
-    case 'c':
-        break;
+        case 2:
+            imprime_dados_cadastrados();
+            break;
 
-    case 'd':
-        printf("Tchau, espero te ver logo!\n");
-        break;
+        case 3:
+            pesquisa_imprime();
+            break;
 
-    default:
-        printf("Escolha uma opcao valida!\n");
-        break;
-    }
+        case 4:
+            printf("\nEspero te ver logo!\n\n");
+            break;
 
-    printf("\n\n\n\n%s", nome[0]);
+        default:
+            printf("\nEscolha uma opcao valida!\n\n");
+            break;
+        }
+    } while (opcao != 4);
 
     return 0;
 }
